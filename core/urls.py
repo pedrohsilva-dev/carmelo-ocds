00d112a36d9d98f13255e898d82fb66e21e1147f
pedrohsilva-dev <pedrohenriquedasilva204@gmail.com>
2026-07-28 22:25:18 -0300

    prod: admin test

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("administrador/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("contas/", include("accounts.urls")),
     path("membros/", include("members.urls")),
     path("carmelo/", include("carmel.urls")),
